3ec3043ca8b3f161bcfa0f0b92625c543ef5524c
Sven Leach <32299254+SvennoNito@users.noreply.github.com>
2023-08-27 14:45:06 +0200

    Cache considers srate, epolen, and channel for spectogram

--- data_handling/load_cache.py
+++ data_handling/load_cache.py
@@ -4,15 +4,25 @@ from signal_processing import *
 
 def load_cache(ui):
     cache       = {}
+    is_same_spectogram_parameters = False
     filename    = f'{ui.filename}.cache.pkl'
     if os.path.exists(filename):
         with open(filename, "rb") as file:
             cache = pickle.load(file)
 
-    # Spectogram
-    if "spectogram_power" not in cache:
-        cache["spectogram_power"], cache["spectogram_freqs"], cache["spectogram_freqsOI"], cache["swa"] = spectogram_wrapper(ui)
+        # Did spectogram parameters change?
+        is_same_spectogram_parameters = (
+            cache["Sampling_rate_hz"] == ui.config[0]["Sampling_rate_hz"] and 
+            cache["Epoch_length_s"] == ui.config[0]["Epoch_length_s"] and
+            cache["Channel_index_for_spectogram"] == ui.config[0]["Channel_index_for_spectogram"]
+            )
 
+    # Spectogram
+    if "spectogram_power" not in cache or not is_same_spectogram_parameters:
+        cache["spectogram_power"], cache["spectogram_freqs"], cache["spectogram_freqsOI"], cache["swa"] = spectogram_wrapper(ui)
+        cache["Sampling_rate_hz"]               = ui.config[0]["Sampling_rate_hz"]
+        cache["Epoch_length_s"]                 = ui.config[0]["Epoch_length_s"]
+        cache["Channel_index_for_spectogram"]   = ui.config[0]["Channel_index_for_spectogram"]
 
     # Move cache to ui
     cache_to_ui(ui, cache)
