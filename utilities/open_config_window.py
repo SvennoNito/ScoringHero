dc75d9adcbcd9717f880c452cb22c4fee829ec31
Sven Leach <32299254+SvennoNito@users.noreply.github.com>
2023-09-02 16:15:53 +0200

    Added configuration

--- utilities/open_config_window.py
+++ utilities/open_config_window.py
@@ -1,10 +1,34 @@
 from widgets import ConfigurationWindow
 from utilities.redraw_gui import redraw_gui
 from data_handling.write_config import write_configuration_file
+from signal_processing.build_times_vector import build_times_vector
+from signal_processing.compute_spectogram import freqs_of_interest_indices
+
 
 def open_config_window(ui):
     ui.ConfigurationWindow = ConfigurationWindow(ui.config)
-    ui.ChannelPage = ui.ConfigurationWindow.return_channel_page()
+    ui.ChannelPage, ui.GeneralPage = ui.ConfigurationWindow.return_page()
     ui.ChannelPage.changesMade.connect(lambda: redraw_gui(ui))
-    ui.ConfigurationWindow.finished.connect(lambda: write_configuration_file(f'{ui.filename}.config.json', ui.config))
+    # ui.GeneralPage.changesMade.connect(lambda: redraw_gui(ui))
+    ui.ConfigurationWindow.finished.connect(lambda: apply_config_changes(ui))
     ui.ConfigurationWindow.show()        
+
+
+def apply_config_changes(ui):
+
+    # Build times vector
+    build_times_vector(ui)
+
+    # Spectogram borders
+    ui.freqsOI = freqs_of_interest_indices(
+        ui.freqs,
+        ui.config[0]["Spectogram_lower_limit_hz"],
+        ui.config[0]["Spectogram_upper_limit_hz"],
+    )
+    ui.SpectogramWidget.draw_spectogram(ui.power, ui.freqs, ui.freqsOI, ui.config)
+
+    # Redraw EEG
+    redraw_gui(ui)
+
+    # Write configuration file
+    write_configuration_file(f'{ui.filename}.config.json', ui.config)
--- widgets/configurationWindow.py
+++ widgets/configurationWindow.py
@@ -14,16 +14,105 @@ class ConfigurationWindow(QDialog):
 
         # Create the pages
         self.channel_page = ChannelConfiguration(config[1])
-        configuration_page = QLabel("Configuration Content")
+        self.general_page = GeneralConfiguration(config[0])
         events_page = QLabel("Events Content")
 
         # Add the pages to the tabs
         self.tabs.addTab(self.channel_page, "Select Channels")
-        self.tabs.addTab(configuration_page, "Configuration")
+        self.tabs.addTab(self.general_page, "Configuration")
         self.tabs.addTab(events_page, "Events")
 
-    def return_channel_page(self):
-        return self.channel_page
+    def return_page(self):
+        return self.channel_page, self.general_page
+
+
+class GeneralConfiguration(QDialog):
+    changesMade = Signal()
+
+    def __init__(self, general_config, parent=None):
+        super().__init__(parent)
+        layout              = QVBoxLayout(self)
+        form_layout         = QFormLayout()
+        self.width_label    = 200
+        self.spinboxes      = {}
+
+        legend = {
+            'Sampling_rate_hz': {
+                'name_pair': None,
+                'label': 'Sampling rate',
+                'unit': ' Hz'
+            },   
+            'Epoch_length_s': {
+                'name_pair': None,
+                'label': 'Epoch length',
+                'unit': ' s'
+            },     
+            'Distance_between_channels_muV': {
+                'name_pair': None,
+                'label': 'Vertical distance between channels',
+                'unit': ' \u03BCV'
+            },                                         
+            'Channel_index_for_spectogram': {
+                'name_pair': None,
+                'label': 'Channel for spectogram',
+                'unit': ''
+            },
+            'Extension_epoch_left_s': {
+                'name_pair': 'Extension_epoch_right_s',
+                'label': 'Extent epoch',
+                'unit': ' s'
+            },                      
+            'Spectogram_lower_limit_hz': {
+                'name_pair': 'Spectogram_upper_limit_hz',
+                'label': 'Spectogram limits',
+                'unit': ' Hz'
+            },  
+            'Area_power_lower_limit_hz': {
+                'name_pair': 'Area_power_upper_limit_hz',
+                'label': 'Periodogram limits',
+                'unit': ' Hz'
+            }                  
+        }
+   
+        for key, value in legend.items():
+
+            labelbox = QLabel(value['label'])
+            labelbox.setAlignment(Qt.AlignRight)
+            labelbox.setFixedWidth(self.width_label)
+
+            spinbox = QDoubleSpinBox(self)
+            spinbox.setMinimum(0)
+            spinbox.setMaximum(10000)
+            spinbox.setDecimals(0)
+            spinbox.setValue(general_config[key]) 
+            spinbox.setSuffix(value['unit'])     
+            spinbox.valueChanged.connect(lambda: self.change_event(general_config))              
+            self.spinboxes[key] = spinbox
+
+            row_layout = QHBoxLayout()
+            row_layout.addWidget(labelbox)
+            row_layout.addWidget(spinbox)            
+
+            if value['name_pair'] is not None:
+                spinbox = QDoubleSpinBox(self)
+                spinbox.setMinimum(0)
+                spinbox.setMaximum(10000)
+                spinbox.setDecimals(0)
+                spinbox.setValue(general_config[value['name_pair']]) 
+                spinbox.setSuffix(value['unit'])    
+                spinbox.valueChanged.connect(lambda: self.change_event(general_config))              
+                self.spinboxes[value['name_pair']] = spinbox    
+                row_layout.addWidget(spinbox)            
+
+            form_layout.addRow(row_layout) 
+                           
+        # Final layout
+        layout.addLayout(form_layout)    
+
+    def change_event(self, general_config):
+        for id, spinbox in self.spinboxes.items():
+            general_config[id] = int(spinbox.value())
+        self.changesMade.emit()                  
 
 
 class ChannelConfiguration(QDialog):
@@ -48,6 +137,8 @@ class ChannelConfiguration(QDialog):
             # Value by which EEG is multiplied
             spinbox = QDoubleSpinBox(self)
             spinbox.setMinimum(0)
+            spinbox.setMaximum(10000)
+            spinbox.setDecimals(0)
             spinbox.setValue(chaninfo['Scaling_factor'])
             spinbox.valueChanged.connect(lambda: self.change_event(channel_config))  
 
@@ -83,5 +174,5 @@ class ChannelConfiguration(QDialog):
         for counter, chaninfo in enumerate(channel_config):
             chaninfo['Channel_color']       = self.color[counter].currentText()
             chaninfo['Display_on_screen']   = self.display[counter].isChecked()
-            chaninfo['Scaling_factor']      = self.scale[counter].value()
+            chaninfo['Scaling_factor']      = int(self.scale[counter].value())
         self.changesMade.emit()        
