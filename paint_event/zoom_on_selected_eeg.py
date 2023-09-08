e762c13c77bbb5fca8fa664bcf5d940cf452ed1d
Sven Leach <32299254+SvennoNito@users.noreply.github.com>
2023-09-08 07:53:07 +0200

    zoom plot holds on

--- paint_event/zoom_on_selected_eeg.py
+++ paint_event/zoom_on_selected_eeg.py
@@ -6,8 +6,20 @@ def zoom_on_selected_eeg(ui):
 
     data, times = ui.PaintEventWidget.selected_data
     times -= times[0]
+
+    if hasattr(ui, 'figure_zoom'):
+        if plt.fignum_exists(ui.figure_zoom.number):        
+            lines = ui.figure_zoom.get_axes()[0].lines
+            plt.close(ui.figure_zoom)
+            ui.figure_zoom, ax = plt.subplots()
+            for line in lines:
+                ax.plot(line.get_xdata(), line.get_ydata())   
+
+        else:
+            ui.figure_zoom, ax = plt.subplots()
+    else:
+        ui.figure_zoom, ax = plt.subplots()
     
-    figure, ax = plt.subplots()
     ax.set_xlabel('Time (seconds)')
     ax.set_ylabel('Amplitude \u03BCV')        
     ax.plot(times, data)
