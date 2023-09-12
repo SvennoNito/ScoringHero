a916938a2dc9f2dafb425cf3e8dccb32f1c2ccd5
Sven Leach <info@svenno.de>
2023-09-12 23:25:55 +0200

    Fix when loading new EEG that events were not resetted

--- scoring/events_to_ui.py
+++ scoring/events_to_ui.py
@@ -1,13 +1,20 @@
 import numpy as np
-
+from widgets import AnnotationContainer
 
 def events_to_ui(ui, events):
-    event_digits = [item["digit"] for item in events]
-    for event_digit in set(event_digits):
-        container_index = np.where(np.array(event_digits) == event_digit)[0].tolist()
-        for container in np.array(events)[container_index]:
-            ui.AnnotationContainer[event_digit].label = container["event"]
-            ui.AnnotationContainer[event_digit].borders.append(
-                [container["start"], container["end"]]
-            )
-            ui.AnnotationContainer[event_digit].epochs.append(container["epoch"])
+    if len(events) > 0:
+        event_digits = [item["digit"] for item in events]
+        for event_digit in set(event_digits):
+            container_index = np.where(np.array(event_digits) == event_digit)[0].tolist()
+            for container in np.array(events)[container_index]:
+                ui.AnnotationContainer[event_digit].label = container["event"]
+                ui.AnnotationContainer[event_digit].borders.append(
+                    [container["start"], container["end"]]
+                )
+                ui.AnnotationContainer[event_digit].epochs.append(container["epoch"])
+
+    else:
+        ui.AnnotationContainer = [
+            AnnotationContainer(colorindex=counter, label=f"F{counter}")
+            for counter in range(10)
+        ]        
