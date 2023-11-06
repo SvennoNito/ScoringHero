c82f22c065837030bf057e55a893670db59501d9
Sven Leach <32299254+SvennoNito@users.noreply.github.com>
2023-11-06 17:28:18 +0100

    Rectangles now show amplitude of EEG only

--- paint_event/rectangle_size.py
+++ paint_event/rectangle_size.py
@@ -1,7 +1,23 @@
+from signal_processing.sample_from_selection import sample_from_selection
+from signal_processing.channel_from_selection import channel_from_selection
+import numpy as np
+
 def rectangle_size(ui, converted_corners, converted_shape):
     if len(converted_shape) > 0:
+
+        # Selected sample points
+        samples, times = sample_from_selection(ui.times, ui.this_epoch, converted_corners[-1])
+
+        # Select channel
+        channel = channel_from_selection(ui.config, converted_corners[-1], converted_shape[-1])
+
+        # Compute power
+        data    = ui.eeg_data[channel][samples]
+        minmax  = max(data) - min(data)
+
         # Amplitude
-        text = f"{round(converted_shape[-1][1], 2)} \u03BCV"
+        # text = f"{round(converted_shape[-1][1], 2)} \u03BCV"
+        text = f"{minmax:.2f} \u03BCV"
 
         corner = converted_corners[-1]
         xposition = corner[0].x()
