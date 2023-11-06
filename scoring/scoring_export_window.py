c4328970727d8e944a28a327d8660e32f11e74eb
Sven Leach <32299254+SvennoNito@users.noreply.github.com>
2023-11-06 16:43:05 +0100

    Save to suggests current filename

--- scoring/scoring_export_window.py
+++ scoring/scoring_export_window.py
@@ -5,7 +5,7 @@ from .write_scoring import write_scoring
 
 def scoring_export_window(ui):
     name_of_scoringfile, _ = QFileDialog.getSaveFileName(
-        None, "Write scoring file", ui.default_data_path, "*json"
+        None, "Write scoring file", os.path.join(ui.default_data_path, f'{ui.filename}.json'), "*json"
     )
     ui.filename, _ = os.path.splitext(name_of_scoringfile)
     ui.default_data_path = os.path.dirname(name_of_scoringfile)
