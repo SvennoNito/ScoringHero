a2b7a0dcb8340e622a5204114acba22d19d50a6b
Sven Leach <32299254+SvennoNito@users.noreply.github.com>
2023-11-06 16:36:49 +0100

    [FIX] Artefacts were copied between files

--- scoring/events_to_ui.py
+++ scoring/events_to_ui.py
@@ -2,6 +2,12 @@ import numpy as np
 from widgets import AnnotationContainer
 
 def events_to_ui(ui, events):
+
+    ui.AnnotationContainer = [
+        AnnotationContainer(colorindex=counter, label=f"F{counter}")
+        for counter in range(10)
+    ]   
+
     if len(events) > 0:
         event_digits = [item["digit"] for item in events]
         for event_digit in set(event_digits):
@@ -11,10 +17,4 @@ def events_to_ui(ui, events):
                 ui.AnnotationContainer[event_digit].borders.append(
                     [container["start"], container["end"]]
                 )
-                ui.AnnotationContainer[event_digit].epochs.append(container["epoch"])
-
-    else:
-        ui.AnnotationContainer = [
-            AnnotationContainer(colorindex=counter, label=f"F{counter}")
-            for counter in range(10)
-        ]        
+                ui.AnnotationContainer[event_digit].epochs.append(container["epoch"])     
