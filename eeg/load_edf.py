6d775d7b4b73e18eba92eb19c38adbb296edd6ce
Sven Leach <32299254+SvennoNito@users.noreply.github.com>
2024-05-23 15:34:15 +0200

    [FIX] Remove scaling

--- eeg/load_edf.py
+++ eeg/load_edf.py
@@ -12,8 +12,8 @@ def load_edf(filename_prefix):
     print(f"Min amplitude: {eeg_data[0].min()}, Max amplitude: {eeg_data[0].max()}")
 
     # MNE by default scales the data in volt I believe?
-    if data.info['chs'][0]['unit'] == 107:
-        data.apply_function(lambda x: x * 1e6)
-        eeg_data = data.get_data()
+    #if data.info['chs'][0]['unit'] == 107:
+    #    data.apply_function(lambda x: x * 1e6)
+    #    eeg_data = data.get_data()
   
     return eeg_data, int(srate), channel_names
