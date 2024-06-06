4bf6a0a5e4101a65bdec9772429f5059447fce29
Sven Leach <32299254+SvennoNito@users.noreply.github.com>
2024-06-06 15:56:16 +0200

    [MOD] Remove EDF scaling

--- eeg/load_edf.py
+++ eeg/load_edf.py
@@ -11,9 +11,9 @@ def load_edf(filename_prefix):
         print(f"Channel: {ch['ch_name']}, Unit: {ch['unit']}")
     print(f"Min amplitude: {eeg_data[0].min()}, Max amplitude: {eeg_data[0].max()}")
 
-    # MNE by default scales the data in volt I believe?
+    # Data can in volt I believe?
     #if data.info['chs'][0]['unit'] == 107:
-    #    data.apply_function(lambda x: x * 1e6)
+    #    data.apply_function(lambda x: x * 1e-6)
     #    eeg_data = data.get_data()
   
     return eeg_data, int(srate), channel_names
