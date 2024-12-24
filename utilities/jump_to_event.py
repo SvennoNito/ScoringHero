847646d3434f5035392fba108cec2a4d5ac8aae1
Sven Leach <info@svenno.de>
2024-12-24 19:10:49 +0100

    [FIX] Event jumps went one epoch too far

--- utilities/jump_to_event.py
+++ utilities/jump_to_event.py
@@ -2,13 +2,17 @@ from itertools import chain
 from .refresh_gui import refresh_gui
 
 def jump_to_event(ui):
-    epochs = []
+    event_epochs = []
     for container in ui.AnnotationContainer:
-        epochs.extend(container.epochs)
-    epochs = list(set(chain(*epochs)))
+        event_epochs.extend(container.epochs)
 
-    next_epoch    = min([epoch for epoch in epochs if epoch > ui.this_epoch+1], default=None)
+    event_epochs     = list(set(chain(*event_epochs)))
+    next_epoch = min([epoch for epoch in event_epochs if epoch > ui.this_epoch+1], default=None)
+
+    if next_epoch is None:
+        next_epoch = min([epoch for epoch in event_epochs if epoch <= ui.this_epoch+1], default=None)
+
     if next_epoch is not None:
-        ui.this_epoch = next_epoch
+        ui.this_epoch = next_epoch - 1
         refresh_gui(ui)
     return
