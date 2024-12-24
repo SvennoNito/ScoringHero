382ae5af74f514095852865dc09cf73fb7a942f3
Sven Leach <info@svenno.de>
2024-12-24 18:10:50 +0100

    [FIX] Fixes event jumps

--- utilities/jump_to_event.py
+++ utilities/jump_to_event.py
@@ -6,9 +6,9 @@ def jump_to_event(ui):
     for container in ui.AnnotationContainer:
         epochs.extend(container.epochs)
     epochs = list(set(chain(*epochs)))
-    next_epochs = [epoch > ui.this_epoch+1 for epoch in epochs]
 
-    if any(next_epochs):
-        ui.this_epoch = epochs[[epoch > ui.this_epoch+1 for epoch in epochs].index(True)] - 1
+    next_epoch    = min([epoch for epoch in epochs if epoch > ui.this_epoch+1], default=None)
+    if next_epoch is not None:
+        ui.this_epoch = next_epoch
         refresh_gui(ui)
     return
