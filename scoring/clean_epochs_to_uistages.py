77dcb4ad54991aa2c37ca154bc4645d1876d22b9
Sven Leach <info@svenno.de>
2024-12-24 15:34:44 +0100

    [FIX] Clean epoch assignment
    Once the version turned to 0.1.0, the code went into another if statement, which may not be appropriate.

--- scoring/clean_epochs_to_uistages.py
+++ scoring/clean_epochs_to_uistages.py
@@ -1,11 +1,13 @@
 def clean_epochs_to_uiscoring(ui, container):
     if container.key == 'A':
-        if ui.version[0] == 0 and ui.version[1] == 0:
-            for epoch, stage in enumerate(ui.stages):
-                stage["clean"] = 0 if any(epoch+1 in container for container in container.epochs) else 1
+        for epoch, stage in enumerate(ui.stages):
+            stage["clean"] = 0 if any(epoch+1 in container for container in container.epochs) else 1
 
-        else:
-            if any(ui.this_epoch+1 in epoch for epoch in container.epochs):
-                stage["clean"] = 0
-            else:
-                stage["clean"] = 1
+        # Honestly not sure whether this is correct, but maybe it would be faster.
+        # For sure the "stage" variable would need to be extracted first, something like ui.stages[something]
+        # But I´m also not sure what happens when several epochs are labeled at once with artefacts. So rather
+        # leave as is.    
+        #if any(ui.this_epoch+1 in epoch for epoch in container.epochs):
+        #    stage["clean"] = 0
+        #else:
+        #    stage["clean"] = 1
