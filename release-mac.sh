0fb7bb771196f1ff219bdcdb028beac68bfe3550
Simon Accascina <simon@accascina.me>
2025-01-05 20:22:14 +0100

    multiarch build

--- release-mac.sh
+++ release-mac.sh
@@ -3,11 +3,12 @@
 set -euo pipefail
 
 pyinstaller --onefile \
---target-arch=x86_64 \
+--target-arch="$TARGET_ARCH" \
 --icon='icon.icns' \
---paths='./.venv/lib/python3.13/site-packages' scoringhero.py \
---add-data='./.venv/lib/python3.13/site-packages/mne:mne' \
+--paths="$VENV_PATH/lib/python3.13/site-packages" scoringhero.py \
+--add-data="$VENV_PATH/lib/python3.13/site-packages/mne:mne" \
 --add-data='./help/images/selection_box.png:help/images' \
 --add-data='./themes/light_theme.qss:themes' \
+--name="$FINAL_FILE_NAME" \
 --hidden-import=decorator
 
