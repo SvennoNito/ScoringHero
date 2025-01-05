276874f69b5b8b3a38dc57ea5dff50a08085b60d
Simon Accascina <simon@accascina.me>
2025-01-05 18:07:24 +0100

    add mac build script

---
+++ release-mac.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pyinstaller --onefile \
+--paths='./.venv/lib/python3.13/site-packages' scoringhero.py \
+--add-data='./.venv/lib/python3.13/site-packages/mne:mne' \
+--add-data='./help/images/selection_box.png:help/images' \
+--add-data='./themes/light_theme.qss:themes' \
+--hidden-import=decorator
+
