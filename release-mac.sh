8383fc837ab901df1e46bd9d040e00ae4785c4b2
Sven Leach <info@svenno.de>
2025-01-07 23:12:12 +0100

    [FIX] Add theme.qss path for pyinstaller

--- release-mac.sh
+++ release-mac.sh
@@ -8,6 +8,7 @@ pyinstaller --onefile \
 --paths="$VENV_PATH/lib/python3.13/site-packages" scoringhero.py \
 --add-data="$VENV_PATH/lib/python3.13/site-packages/mne:mne" \
 --add-data='./help/images/selection_box.png:help/images' \
+--add-data="./style/modern_theme.qss:style" \
 --name="$FINAL_FILE_NAME" \
 --hidden-import=decorator
 
