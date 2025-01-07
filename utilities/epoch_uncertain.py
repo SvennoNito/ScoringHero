60e8773712c10d13c75967b70d410f5a8ddc7a21
Sven Leach <info@svenno.de>
2025-01-07 23:00:56 +0100

    [FIX] Next uncertain epoch now cycles through all epochs

--- utilities/epoch_uncertain.py
+++ utilities/epoch_uncertain.py
@@ -11,9 +11,17 @@ def first_uncertain_stage(ui):
 
 
 def next_uncertain_stage(ui):
-    for epoch in range(ui.this_epoch+1, ui.numepo):
-        if ui.stages[epoch]["confidence"] != None:
-            if ui.stages[epoch]["confidence"] < .5:
-                ui.this_epoch = epoch
-                refresh_gui(ui)
-                return
+    uncertain_epochs = [
+        epoch for epoch, stage in enumerate(ui.stages)
+        if stage["confidence"] is not None and stage["confidence"] < 0.5
+    ]
+    next_epoch = min([epoch for epoch in uncertain_epochs if epoch > ui.this_epoch], default=None)
+
+    if next_epoch is None:
+        next_epoch = min([epoch for epoch in uncertain_epochs if epoch <= ui.this_epoch], default=None)
+
+    if next_epoch is not None:
+        ui.this_epoch = next_epoch
+        refresh_gui(ui)
+    return
+
