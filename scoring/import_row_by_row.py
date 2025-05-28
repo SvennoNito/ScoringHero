6c8389bcdeef29fdf63053bbc381445867bd6fb2
Sven Leach <info@svenno.de>
2025-05-29 01:55:30 +0200

    [FIX] Corrected the YASA import logic

---
+++ scoring/import_row_by_row.py
@@ -0,0 +1,38 @@
+import re, warnings
+
+def import_row_by_row(string_to_check, lines, mapping_str, mapping_num, numepo):
+    """
+    This function is a placeholder for importing rows.
+    It currently does nothing but can be expanded in the future.
+    """
+
+    scoring_str = []
+    scoring_num = []    
+    equality_check = False
+
+    for row, item in enumerate(lines):
+        item            = item.strip()
+        found_chars     = re.findall(string_to_check, item)
+
+        if not found_chars:  # skip rows
+            print(f"Row {row} in scoringfile reads '{item}. None of the specified strings '{string_to_check}' was found. Skipping this row.")
+            continue                
+
+        if len(scoring_str) - numepo == 0 and equality_check:
+            total_lines = len(lines) 
+            raise ValueError(
+                f"Error! Imported sleep scoring file has MANY more epochs ({total_lines}) than expected ({numepo}). Was the scoring performed with the same epoch length as currently displayed in scoringhero?",
+            )   
+
+        if len(scoring_str) - numepo == 0:
+            warnings.warn(
+                f"Warning! Imported sleep scoring file has 1 more epoch than expected. Scoringhero discards epochs with only partial data present. Hence, last scored epoch will be ignored.",        
+                UserWarning
+            )
+            equality_check = True
+            continue	              
+        
+        scoring_str.append(mapping_str[found_chars[0]])
+        scoring_num.append(mapping_num[scoring_str[-1]])        
+
+    return scoring_str, scoring_num
--- scoring/load_yasa.py
+++ scoring/load_yasa.py
@@ -1,5 +1,6 @@
 import os, json, re, warnings
 from .default_scoring import default_scoring
+from .import_row_by_row import import_row_by_row
 import numpy as np
 
 def load_yasa(scoring_filename, epolen, numepo):
@@ -32,33 +33,10 @@ def load_yasa(scoring_filename, epolen, numepo):
     if os.path.exists(scoring_filename):
         with open(scoring_filename, "r") as file:
             lines = file.readlines()       
-            for row, item in enumerate(lines):
-                string_to_check = r'N[123]|NREM[123]|WAKE|REM|W|R|[01234]'	
-                found_chars     = re.findall(string_to_check, item)
-
-                if not found_chars:  # skip rows
-                    print(f"Row {row} in {scoring_filename} reads '{item.strip()}. None of the specified strings '{string_to_check}' was found. Skipping this row.")
-                    continue
-
-                if row >= numepo:  # sanity check
-                    total_lines = len(lines)
-                    additional_lines_threshold = 100
-                    if total_lines >= numepo + additional_lines_threshold:
-                        raise ValueError(
-                        f"Error! Imported sleep scoring file has MUCH more epochs ({total_lines}) than expected ({numepo}). When the file has {additional_lines_threshold} lines more than expected, it is assumed that something is off. Was the scoring performed with the same epoch length as currently displayed in scoringhero?",
-                        )
-                    else:
-                        warnings.warn(
-                            f"Warning! Imported sleep scoring file has more epochs ({total_lines}) than expected ({numepo}). Only the first {numepo} scored epochs will be used.",        
-                            UserWarning
-                        )
-                    break        
-
-                scoring_str.append(mapping_str[found_chars[0]])
-                scoring_num.append(mapping_num[scoring_str[-1]])
+ 
+            [scoring_str, scoring_num] = import_row_by_row(r'N[123]|NREM[123]|WAKE|REM|W|R|[01234]', lines, mapping_str, mapping_num, numepo)        
 
             scoring_data = default_scoring(epolen, numepo)
-
 
             for counter, (str, num) in enumerate(zip(scoring_str, scoring_num)):
                 scoring_data[counter]["digit"]  = int(num)
