bb8d1140f705ab0ec8ab6db3d4fd3345416ca850
Sven Leach <info@svenno.de>
2025-05-29 01:02:22 +0200

    [NEW] YASA scoring now allows to import numerical stages.
    Improved warnings and error messages

--- scoring/load_yasa.py
+++ scoring/load_yasa.py
@@ -1,4 +1,4 @@
-import os, json, re
+import os, json, re, warnings
 from .default_scoring import default_scoring
 import numpy as np
 
@@ -7,15 +7,20 @@ def load_yasa(scoring_filename, epolen, numepo):
     mapping_str = {'W': 'Wake', 
                    'Wake': 'Wake', 
                    'WAKE': 'Wake', 
+                   '0': 'Wake',                   
                    'N1': 'N1', 
                    'NREM1': 'N1',
+                   '1': 'N1',
                    'N2': 'N2', 
                    'NREM2': 'N2', 
+                   '2': 'N2',
                    'N3': 'N3', 
+                   '3': 'N3',
                    'NREM3': 'N3', 
                    'R': 'REM',
                    'Rem': 'REM',
-                   'REM': 'REM'}
+                   'REM': 'REM',
+                   '4': 'REM'}
     mapping_num = {'Wake': 1, 
                    'N1': -1, 
                    'N2': -2, 
@@ -26,9 +31,29 @@ def load_yasa(scoring_filename, epolen, numepo):
 
     if os.path.exists(scoring_filename):
         with open(scoring_filename, "r") as file:
-            lines = file.readlines()
-            for item in lines:
-                found_chars  = re.findall(r'N[123]|NREM[123]|[WR]|[WAKE]|[REM]', item)
+            lines = file.readlines()       
+            for row, item in enumerate(lines):
+                string_to_check = r'N[123]|NREM[123]|WAKE|REM|W|R|[01234]'	
+                found_chars     = re.findall(string_to_check, item)
+
+                if not found_chars:  # skip rows
+                    print(f"Row {row} in {scoring_filename} reads '{item.strip()}. None of the specified strings '{string_to_check}' was found. Skipping this row.")
+                    continue
+
+                if row >= numepo:  # sanity check
+                    total_lines = len(lines)
+                    additional_lines_threshold = 100
+                    if total_lines >= numepo + additional_lines_threshold:
+                        raise ValueError(
+                        f"Error! Imported sleep scoring file has MUCH more epochs ({total_lines}) than expected ({numepo}). When the file has {additional_lines_threshold} lines more than expected, it is assumed that something is off. Was the scoring performed with the same epoch length as currently displayed in scoringhero?",
+                        )
+                    else:
+                        warnings.warn(
+                            f"Warning! Imported sleep scoring file has more epochs ({total_lines}) than expected ({numepo}). Only the first {numepo} scored epochs will be used.",        
+                            UserWarning
+                        )
+                    break        
+
                 scoring_str.append(mapping_str[found_chars[0]])
                 scoring_num.append(mapping_num[scoring_str[-1]])
 
