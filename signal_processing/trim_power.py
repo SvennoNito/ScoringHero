0f687ae893c315258344d2fb6fd8d7a29952c377
Sven Leach <info@svenno.de>
2026-03-14 09:31:22 +0100

    [FIX] Eliminate Duplicate Mask in trim_power

--- signal_processing/trim_power.py
+++ signal_processing/trim_power.py
@@ -2,6 +2,5 @@ import numpy as np
 
 
 def trim_power(power, freqs, lower_limit, upper_limit):
-    power = power[np.where((freqs <= upper_limit) & (freqs >= lower_limit))]
-    freqs = freqs[np.where((freqs <= upper_limit) & (freqs >= lower_limit))]
-    return power, freqs
+    mask = (freqs >= lower_limit) & (freqs <= upper_limit)
+    return power[mask], freqs[mask]
