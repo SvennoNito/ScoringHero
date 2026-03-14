5991464f5f680cc5cd0014343307f28aa4bffc5e
Sven Leach <info@svenno.de>
2026-03-14 09:21:18 +0100

    [NEW] call_tf_widget

--- utilities/refresh_gui.py
+++ utilities/refresh_gui.py
@@ -1,6 +1,7 @@
 from signal_processing.trim_power import trim_power
 from signal_processing.min_max_scale import min_max_scale
 from events.draw_event_in_this_epoch import draw_event_in_this_epoch
+from utilities.tf_config_helper import call_tf_widget
 
 
 def refresh_gui(ui):
@@ -33,21 +34,7 @@ def refresh_gui(ui):
     ui.RectanglePower.update_powerline(freqs, power)
 
     # Update time-frequency panel
-    srate = ui.config[0]["Sampling_rate_hz"]
-    display_mode = ui.config[0].get("Wavelet_display_mode", "Z-scored Power")
-    freq_scale = ui.config[0].get("Wavelet_frequency_scale", "Logarithmic")
-    freq_limits = ui.config[0].get("Wavelet_frequency_limits_hz", None)
-    time_unit = ui.config[0].get("EEG_panel_time_unit", "Seconds")
-    epoch_length = ui.config[0]["Epoch_length_s"]
-    tf_channel_label = ui.config[0].get("Wavelet_channel", "")
-    channel_labels = [ch["Channel_name"] for ch in ui.config[1]]
-    tf_channel_idx = channel_labels.index(tf_channel_label) if tf_channel_label in channel_labels else 0
-    power_limits = ui.config[0].get("Wavelet_power_limits", None)
-    ui.TFWidget.update_tf(ui.eeg_data, ui.times, ui.this_epoch, srate, ui.tf_freqs,
-                          ui.tf_norm_median, ui.tf_norm_iqr, ui.tf_norm_rms, ui.tf_norm_median_linear,
-                          display_mode, freq_scale, freq_limits,
-                          time_unit, epoch_length, tf_channel_idx, tf_channel_label,
-                          power_limits)
+    call_tf_widget(ui)
 
     # Draw annotations
     for container in ui.AnnotationContainer:
---
+++ utilities/tf_config_helper.py
@@ -0,0 +1,16 @@
+def call_tf_widget(ui):
+    srate = ui.config[0]["Sampling_rate_hz"]
+    display_mode = ui.config[0].get("Wavelet_display_mode", "Z-scored Power")
+    freq_scale = ui.config[0].get("Wavelet_frequency_scale", "Logarithmic")
+    freq_limits = ui.config[0].get("Wavelet_frequency_limits_hz", None)
+    time_unit = ui.config[0].get("EEG_panel_time_unit", "Seconds")
+    epoch_length = ui.config[0]["Epoch_length_s"]
+    tf_channel_label = ui.config[0].get("Wavelet_channel", "")
+    channel_labels = [ch["Channel_name"] for ch in ui.config[1]]
+    tf_channel_idx = channel_labels.index(tf_channel_label) if tf_channel_label in channel_labels else 0
+    power_limits = ui.config[0].get("Wavelet_power_limits", None)
+    ui.TFWidget.update_tf(ui.eeg_data, ui.times, ui.this_epoch, srate, ui.tf_freqs,
+                          ui.tf_norm_median, ui.tf_norm_iqr, ui.tf_norm_rms, ui.tf_norm_median_linear,
+                          display_mode, freq_scale, freq_limits,
+                          time_unit, epoch_length, tf_channel_idx, tf_channel_label,
+                          power_limits)
