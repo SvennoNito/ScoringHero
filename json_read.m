690b3bec28770be44526166fbff497d7c7e4d522
Sven Leach <info@svenno.de>
2026-03-15 13:57:30 +0100

    Delete json_read.m

--- json_read.m
+++
@@ -1,5 +0,0 @@
-fid = fopen('C:\PhDScripts\Sides\ScoringHero\example_data\example_data.json', 'rt');
-rawData = fread(fid, inf, '*char');
-fclose(fid);
-strData = string(rawData');
-scoring = jsondecode(strData);
