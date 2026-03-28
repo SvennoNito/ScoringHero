c6695a33ea943a01fdf29576a39583724e1f85bb
Sven Leach <info@svenno.de>
2026-03-28 13:34:57 +0100

    [NEW] auto-scoringhero

--- build-win-gssc.bat
+++ build-win-gssc.bat
@@ -1,6 +1,7 @@
 @echo off
-set UV_PROJECT_ENVIRONMENT=.venv_build
-uv run --python 3.13 --extra build-win python -m nuitka ^
+set UV_PROJECT_ENVIRONMENT=.venv_gssc
+uv sync --python 3.13 --extra build-win --extra gssc
+uv run --python 3.13 python -m nuitka ^
     --onefile ^
     --jobs=8 ^
     --windows-icon-from-ico=icon.ico ^
@@ -10,9 +11,12 @@ uv run --python 3.13 --extra build-win python -m nuitka ^
     --include-module=PySide6.QtOpenGLWidgets ^
     --include-module=widgets.filterWindow ^
     --include-module=widgets.gsscWindow ^
+    --include-package=gssc ^
+    --include-package=mne ^
+    --include-package=torch ^
     --include-data-files=./help/images/selection_box.png=help/images/selection_box.png ^
     --include-data-files=./style/modern_theme.qss=style/modern_theme.qss ^
     --include-data-files=./spectral.txt=spectral.txt ^
-    --output-filename=scoringhero_windows.exe ^
+    --output-filename=auto-scoringhero_windows.exe ^
     --output-dir=dist ^
     scoringhero.py
