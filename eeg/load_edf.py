2eb909e3267ddcf7c938ce62f5df0e148ffb6536
Sven Leach <info@svenno.de>
2026-03-28 08:28:03 +0100

    [FIX] For importing Click EDF files

--- eeg/load_edf.py
+++ eeg/load_edf.py
@@ -7,7 +7,12 @@ def load_edf(filename_prefix, scale_to_uv=False):
     edf = _read_edf(file)
     srate = int(edf.signals[0].sampling_frequency)
     channel_names = [s.label for s in edf.signals]
-    eeg_data = np.array([s.data for s in edf.signals])
+    signals = [s.data for s in edf.signals]
+    max_len = max(len(s) for s in signals)
+    eeg_data = np.array([
+        np.pad(s, (0, max_len - len(s))) if len(s) < max_len else s
+        for s in signals
+    ])
     if scale_to_uv:
         eeg_data = eeg_data * 1e6
     return eeg_data, srate, channel_names
